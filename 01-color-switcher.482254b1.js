1a065eedd6f2de80e4f520e369d2863bccbfeaba
SerhyRymar <108816720+SerhyRymar@users.noreply.github.com>
2022-12-04 11:12:27 +0000

    Deploying to gh-pages from @ SerhyRymar/goit-js-hw-09@02f11d314847e11e4aa40bea939f9c40867e7251 🚀

---
+++ 01-color-switcher.482254b1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),i=document.querySelector("body"),r={intervalId:null,isActive:!1,start(){this.isActive||(this.isActive=!0,this.intervalId=setInterval((()=>{i.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0)},stop(){clearInterval(this.intervalId),this.isActive=!1,t.disabled=!1}};t.addEventListener("click",(()=>{r.start()})),e.addEventListener("click",(()=>{r.stop()}));
+//# sourceMappingURL=01-color-switcher.482254b1.js.map
